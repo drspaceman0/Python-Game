ccedff1093a974a8d7933eab0ff0a15908ddf1a3
Eric <ericmrsh9@gmail.com>
2016-05-02 21:16:05 -0700

    Fixed a syntax issue.

--- Game.py
+++ Game.py
@@ -15,43 +15,6 @@ import Player
 import Input
 
 				 
-
-	
-<<<<<<< HEAD
-=======
-	
-
-		
-		
-def load_image(name):
-    image = pygame.image.load(name)
-    return image
-
-# http://stackoverflow.com/questions/14044147/animated-sprite-from-few-images
-class Sprite(pygame.sprite.Sprite):
-
-    def __init__(self):
-		super(Sprite, self).__init__()
-		self.counter = 0
-		self.images = []
-		self.images.append(load_image('images\player_idle1.png'))
-		self.images.append(load_image('images\player_idle2.png'))
-		# assuming both images are 64x64 pixels
-
-		self.index = 0
-		self.image = self.images[self.index]
-		self.rect = pygame.Rect(5, 5, 64, 64)
-
-    def update(self, x, y):
-		self.counter += 1 
-		self.rect = pygame.Rect(x, y, 64, 64) 
-		if self.counter > 10: # after ten clicks switch sprites
-			self.counter = 0
-			self.index += 1
-			if self.index >= len(self.images):
-				self.index = 0
-			self.image = self.images[self.index]
->>>>>>> 8c0a8f5fd8073ade00b9cffc4bba49d1d0785ed9
 #		
 # START GAME
 #	

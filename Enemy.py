969b6fcbc417537fb571924ea4d1d2298a68ef98
John Snevily <snev7821@vandals.uidaho.edu>
2016-05-02 21:31:51 -0700

    Updated collision and enemy health. Also updated chase location of enemy class

--- Enemy.py
+++ Enemy.py
@@ -11,7 +11,7 @@ class Enemy:
 		self.x = posx
 		self.y = posy
 		self.size = size
-		self.health = size/2
+		self.health = size/4
 		self.color = Display.GREEN
 		self.speed = 5
 		Enemy.num_enemies += 1
@@ -19,23 +19,24 @@ class Enemy:
 		
 	def update(self, PlayerObj):
 		for enemy in Enemy.listEnemies:
-			'''for bullet in Bullet.Bullet.listBullets:
-				if bullet.collision(enemy.x, enemy.y):
+			for bullet in Bullet.Bullet.listBullets:
+				if Enemy.collision(enemy, bullet):
 					bullet.delete()
+					enemy.health-= 5
 					if enemy.health < 0:
-						enemy.death()'''
+						enemy.death()
 			if enemy.x >= Display.SCREEN_WIDTH or enemy.x <= 0 or enemy.y >= Display.SCREEN_HEIGHT or enemy.y <= 0:
 				Enemy.delete(enemy)
 				continue
 			#Chase the player in the x direction
-			if (PlayerObj.x > enemy.x):
+			if (PlayerObj.x+20) > enemy.x:
 				enemy.x = enemy.x + enemy.speed
-			if (PlayerObj.x < enemy.x):
+			if (PlayerObj.x+20) < enemy.x:
 				enemy.x = enemy.x - enemy.speed
 			#Then chase the player in the y direction
-			if (PlayerObj.y > enemy.y):
+			if (PlayerObj.y+20) > enemy.y:
 				enemy.y = enemy.y + enemy.speed
-			if (PlayerObj.y < enemy.y):
+			if (PlayerObj.y+20) < enemy.y:
 				enemy.y = enemy.y - enemy.speed
 			#Draw the enemies	
 			pygame.draw.circle(Display.DISPLAYSURF, Display.GREEN, (enemy.x, enemy.y), enemy.size)
@@ -55,7 +56,7 @@ class Enemy:
 			return True
 		
 				
-				
+	'''			
 	def bulletCollide(self, bulletlist):
 		for enemy in Enemy.listEnemies:
 			for Bullet.Bullet.bullet in bulletlist:
@@ -63,7 +64,7 @@ class Enemy:
 					Bullet.Bullet.bullet.delete()
 					enemy.health -= 5
 				if enemy.health <= 0:
-					enemy.death()
+					enemy.death()'''
 	
 	def delete(self):
 		Enemy.num_enemies -= 1

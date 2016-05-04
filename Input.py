f1571cf956920b20171f6a67c0cba6b26212e489
Eric <ericmrsh9@gmail.com>
2016-05-03 18:37:30 -0700

    Cleaned up the sprite class, gave player 4 directional sprites

--- Input.py
+++ Input.py
@@ -50,9 +50,9 @@ def terminate():
 	sys.exit()
 
 def checkForQuit():
-     for event in pygame.event.get(QUIT): # get all the QUIT events
-         terminate() # terminate if any QUIT events are present
-     for event in pygame.event.get(KEYUP): # get all the KEYUP events
-         if event.key == K_ESCAPE:
-             terminate() # terminate if the KEYUP event was for the Esc key
-         pygame.event.post(event) # put the other KEYUP event objects back
+	 for event in pygame.event.get(QUIT): # get all the QUIT events
+		 terminate() # terminate if any QUIT events are present
+	 for event in pygame.event.get(KEYUP): # get all the KEYUP events
+		 if event.key == K_ESCAPE:
+			 terminate() # terminate if the KEYUP event was for the Esc key
+		 pygame.event.post(event) # put the other KEYUP event objects back

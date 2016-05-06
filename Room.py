ff2d799bcc19877db2bb81d2a8058cd4443c8f5b
Eric <ericmrsh9@gmail.com>
2016-05-06 12:17:44 -0700

    several small changes

--- Room.py
+++ Room.py
@@ -2,7 +2,10 @@ import pygame
 import math
 import Display
 import Enemy
-'''
+
 class Room:
+	numRooms = 0
+	listRooms = []
 	def __init__(self):
-'''
+		self.width = SCREEN_WIDTH
+		self.height = SCREEN_HEIGHT

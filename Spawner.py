98a1d75c5a4bd1e320d69a2535c0063df2d01036
John Snevily <snev7821@vandals.uidaho.edu>
2016-05-06 10:31:51 -0700

    balancing spawn values

--- Spawner.py
+++ Spawner.py
@@ -5,7 +5,7 @@ import Player
 import Display
 import GooEnemy
 import BrickEnemy
-MAXENEMY = 6 #for testing - not set in stone, despite being a constant. maybe by difficulty?
+MAXENEMY = 10 #for testing - not set in stone, despite being a constant. maybe by difficulty?
 ENEMYGOOSIZE = 30
 ENEMYBRICKSIZE = 20
 EDGING = 30 #Used to account for the edge of the displaysurf so they don't delete as spawned

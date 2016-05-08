10db72f33b31d41f233d25e73ae8adf59c1d80e9
John Snevily <snev7821@vandals.uidaho.edu>
2016-05-07 19:38:14 -0700

    in-progress update for information flow

--- LabelMaker.py
+++ LabelMaker.py
@@ -1,12 +1,21 @@
+'''The idea here is to only use one enemy class and make them highly extensible/variable based on 
+	what they get for adjectives, nouns, and verbs. Color will add even more variety. The best part about this,
+	is that a matching label class can be made to modify the single enemy class instead of creating new
+	enemies for each type '''
+
 
 #singleton...?
 class LabelMaker:
 	adjectives = []
 	nouns = []
 	verbs = []
+	colors = []
 	
-	def __init__(self)
+	def __init__(self):
 		#Should also make a matching attribute class, which changes enemies based on traits
+		
+		#Define Adjectives
+		#These change enemy SPEED, SIZE, DAMAGE and DROP_RATE, as well as enemy.misc ATTACK, PATH_LOGIC
 		adjectives.append("fierce") #Charge/doesn't do much
 		adjectives.append("monstrous") #Size*=2
 		adjectives.append("swift") #speed*=2
@@ -19,6 +28,28 @@ class LabelMaker:
 		adjectives.append("cowardly") #runs away from player in certain range
 		adjectives.append("zombified") #slower, but more health
 		adjectives.append("slimy") #slows player  
+		adjectives.append("rich") #Double drop rate - for hard
+		adjectives.append("poor") #Halves drop rate - for easy 
+		adjectives.append("ugly") #nothing/descriptor
+		adjectives.append("") #Use this as a template for more additions
+		
+		
+		
+		
+		#Define Nouns:
+		nouns.append("Goo") # Green-
+		nouns.append("Swarm") #
+		nouns.append("Cow") #
+		nouns.append("Golem") #
+		nouns.append("Wizard") #
+		nouns.append("Bandit") #
+		nouns.append("Fighter") #
+		nouns.append("Skeleton") #
+		nouns.append("Skull") #
+		nouns.append("Orc") #
+		nouns.append("Demon") #
+		nouns.append("Goblin") #
+		nouns.append("") #Template
 		
 		
 		
---
+++ hypotheticalenemy.py
@@ -0,0 +1,12 @@
+#Hypothetical enemy class
+
+#ADJECTIVE TRAITS
+	SIZE
+	SPEED
+	ATTACK #type, melee, ranged, suicide, etc.
+	DAMAGE #number of player hitpoints to deduct
+	
+#NOUN TRAITS
+	COLOR
+	SPRITE #if we use this, could be lots of code/images
+	

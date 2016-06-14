ad07a0814021a36c9e805a5b30246b80b921ae09
John Snevily <snev7821@vandals.uidaho.edu>
2016-06-14 09:36:37 -0700

    fixed bug where arrow hit more than once

--- RangedWeapon.py
+++ RangedWeapon.py
@@ -66,6 +66,7 @@ class RangedWeapon:
 					for enemy in self.dung.returnCurrentRoom().enemylist:
 						if math.sqrt(pow(arrow.x - enemy.x, 2) + pow(arrow.y - enemy.y, 2)) < 30:
 							enemy.health -= self.damage
+							arrow.exists = False
 							print "%s has been hit with an arrow for %s damage!" % (enemy.name, self.damage)
 							if enemy.health <= 0:
 								enemy.death()
